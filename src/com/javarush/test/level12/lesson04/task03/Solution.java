933e444766ca1cc92b7cf8d9a68868fefac7bbfb
Andrey Chernyak <chernyak.andrew@gmail.com>
2015-03-03 19:08:32 +0200

    Resolved :  level12, lesson04, task01-05

---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public String print(String s){
+        return s;
+    }
+
+    public Integer print(Integer s){
+        return s;
+    }
+
+    public int print(int s){
+        return s;
+    }
+
+    public boolean print(boolean bool){
+        return bool;
+    }
+
+    public void print(){
+    }
+
+}
