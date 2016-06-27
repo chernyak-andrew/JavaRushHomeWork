6dc17a43edc5a2940b66dd669dad1c35d1256831
chernyak-andrew <Knighthood13>
2016-06-27 19:57:18 +0300

    Resolved :  level04, lesson16, task 02

--- src/com/javarush/test/level04/lesson16/home02/Solution.java
+++ src/com/javarush/test/level04/lesson16/home02/Solution.java
@@ -13,23 +13,36 @@ public class Solution
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        int firstNumber = Integer.parseInt(reader.readLine());
+        int secondNumber = Integer.parseInt(reader.readLine());
+        int thirdNumber = Integer.parseInt(reader.readLine());
+
+        int max = getMax(firstNumber, getMax(secondNumber,thirdNumber));
+
+        if (max!=getMax(firstNumber, secondNumber))
+            System.out.println(getMax(firstNumber, secondNumber));
+        else if(max!=getMax(secondNumber, thirdNumber))
+            System.out.println(getMax(secondNumber, thirdNumber));
+        else
+            System.out.println(getMax(firstNumber, thirdNumber));
+
+       /* BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
         int c = Integer.parseInt(reader.readLine());
-
-
-        if (a > b && b < c)
-        {
-            System.out.println(c);
-
-        } else if (a > b && b > c)
-        {
-            System.out.println(b);
-        } else
-        {
-            System.out.println(c);
-        }
+        int d = a;
+        if ((c < a && c > b) || (c < b && c > a) ) d = c;
+        if ((b < c && b > a) || (b < a && b > c) ) d = b;
+        System.out.println(d);*/
 
     }
 
- }
+    private static int getMax(int a, int b)
+    {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+}
+
