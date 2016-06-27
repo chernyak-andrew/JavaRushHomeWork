e7b9cf96c1174d553e2fbfffbe5e2dd1aa69067d
chernyak-andrew <Knighthood13>
2016-06-27 19:56:58 +0300

    Resolved :  level04, lesson13, task 03

--- src/com/javarush/test/level04/lesson13/task03/Solution.java
+++ src/com/javarush/test/level04/lesson13/task03/Solution.java
@@ -1,8 +1,5 @@
 package com.javarush.test.level04.lesson13.task03;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 /* Рисуем треугольник
 Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
 Пример:
